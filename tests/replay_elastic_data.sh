d8eaf61dee1591d185b30f543dd172221b0af3fc
Whitney Armstrong <warmstrong@anl.gov>
2019-08-15 18:50:09 -0500

    modified:   .gitlab-ci.yml 	modified:   tests/elastic_test.sh 	new file:   tests/elastic_test2.sh 	modified:   tests/my_root_script.cxx 	new file:   tests/replay_elastic_data.sh

---
+++ tests/replay_elastic_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "This is the elastic testing..."
+echo " "
+echo "There are currently 0 tests to run!"
+which hcana
+
+ls -lrth
+ls -lrth build
+
+git clone git@eicweb.phy.anl.gov:jlab/hallc/exp/CSV/hallc_replay_csv.git
+git clone git@eicweb.phy.anl.gov:jlab/hallc/exp/CSV/online_csv.git
+
+cd online_csv 
+ln -s ../hallc_reaply_csv/PARAM
+# and the reset
+
+mkdir raw 
+pushd raw
+  wget coin.dat
+popd
+
+
+# run replay script
+
+echo " hcana calls... the coin replay script and outputs blah.root"
