c02a7561cb6f7c370d728e6741dde9b6c9c8d480
Whitney Armstrong <warmstrong@anl.gov>
2019-08-16 16:11:29 -0500

    modified:   setup.sh

--- .gitlabci/setup.sh
+++ .gitlabci/setup.sh
@@ -9,33 +9,26 @@ apt-get update && apt-get install -y wget git \
                                           automake \
                                           autoconf \
                                           uuid-dev \
+                                          python3   \
                                           libssl-dev
 
 
 sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
-# Install go
-export VERSION=1.12 OS=linux ARCH=amd64 && \  # Replace the values as needed
-  wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
-  sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
-  rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
-
-echo 'export PATH=/usr/local/go/bin:$PATH' >> /etc/profile.d/go.sh && source /etc/profile.d/go.sh
-
-# Install Singularity
-export VERSION=3.2.0 && # adjust this as necessary \
-    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-    tar -xzf singularity-${VERSION}.tar.gz && \
-    cd singularity
-
-./mconfig && \
-    make -C builddir && \
-    sudo make -C builddir install
-
 # Check Python
 echo "Python Version:"
+which python
 python --version
 pip install sregistry[all]
 sregistry version
 
+echo "sregistry Version:"
 
+# Install Singularity
+
+cd /tmp && \
+    git clone -b vault/release-2.5 https://www.github.com/sylabs/singularity.git
+    cd singularity && \
+    ./autogen.sh && \
+    ./configure --prefix=/usr/local && \
+    make -j8 && make install
