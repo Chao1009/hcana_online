9b314618dd98053acba2480297a74e92c2afa952
Whitney Armstrong <warmstrong@anl.gov>
2019-08-16 01:41:08 -0500

    modified:   replay_elastic_data.sh

--- tests/replay_elastic_data.sh
+++ tests/replay_elastic_data.sh
@@ -27,7 +27,7 @@ ln -s ../ROOTfiles
 
 # run replay script
 
-singularity exec ../build/Singularity.hcana.simg hcana -b -q "scripts/replay_production_coin.cxx+(6012,-1)"
+singularity exec ../build/Singularity.hcana.simg hcana -b -q "SCRIPTS/COIN/PRODUCTION/replay_production_coin_hElec_pProt.C+(6012,-1)"
 
 echo "hcana calls... the coin replay script and outputs blah.root"
 
